8393ed4b31491aa644da404030f692a8a12f68ec
Stian Håklev <shaklev@gmail.com>
2016-02-08 18:10:31 -0500

    Arbitrarily defined forms with validation and Bootstrap work

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
+import jQuery from 'jquery'
 import React from 'react';
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import configStore from './store';
 import { IndexRoute, browserHistory, Router, Route, Link } from 'react-router'
-
+import Bootstrap from 'bootstrap-webpack'
 import VisibleTodoList from './components/VisibleTodoList'
 import Footer from './components/Footer'
 import { BoxWrapper } from './components/Boxes'
 import Drag from './components/Drag'
+import Forms from './components/Forms'
 
 const boxlist = []
 // [{_id: "1", title:"Hi", x:30, y:40},
@@ -37,6 +39,7 @@ render(
     <IndexRoute component='Footer'/>
       <Route path="/test" component={Drag} />
       <Route path="/inbox" component={Test} />
+      <Route path="/forms" component={Forms} />
     </Route>
   </Router>
 
--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,12 @@ module.exports = {
   ],
   module: {
     loaders: [
-      {
+    { test: require.resolve("jquery"), loader: "expose?jQuery" },
+     {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+       {
         loader: "babel",
 
       // Only run `.js` and `.jsx` files through Babel
