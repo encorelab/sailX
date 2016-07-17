b244c9eed26fad05ebe3b7f1037d4be38e9070fe
Stian Håklev <shaklev@gmail.com>
2016-07-17 19:14:53 -0400

    Implementing basic read API functionality, including student/1/COs/ckx

--- login_backend/src/index.js
+++ login_backend/src/index.js
@@ -3,11 +3,41 @@ import { sign } from 'jsonwebtoken'
 import jwt from 'express-jwt'
 import unless from 'express-unless'
 import cors from 'cors'
+import fixtures from './fixtures'
+import rethinkdb from 'rethinkdbdash'
+
+const r = rethinkdb()
+const rr = r.db('encore_login')
 
 const app = express()
-
 app.use(cors())
+app.use(express.static('public'));
 
+app.get('/classes', (req, res) => {
+    rr.table('classes').then((e) => res.json(e))
+})
+
+app.get('/class/:id', (req, res) => {
+    rr.table('classes').get(Number(req.params.id)).then((e) => res.json(e))
+})
+
+app.get('/class/:id/students', (req, res) => {
+    rr.table('students').filter({class: Number(req.params.id)}).then((e) => res.json(e))
+})
+
+app.get('/student/:id', (req, res) => {
+    rr.table('students').get(Number(req.params.id)).then((e) => res.json(e))
+})
+
+app.get('/student/:id/COs', (req, res) => {
+   getCOs(Number(req.params.id), undefined, (e) => res.json(e))
+})
+
+app.get('/student/:id/COs/:type', (req, res) => {
+   getCOs(Number(req.params.id), req.params.type, (e) => res.json(e))
+})
+
+// related to jwt experiments
 app.get('/sign/:user', (req, res) => {
     res.json({req: req.params, token: sign(req.params.user, 'secret')})
 })
@@ -19,8 +49,21 @@ app.get('/', (req, res) => {
     res.json({params: req.params, user: req.user})
 })
 
+// ---------------------------------------------------------------------------------
 
-app.use(express.static('public'));
+fixtures(r)
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 })
+
+
+const getCOs = (studentid, type, returnfn) => {
+    const filter = type ? { type } : {}
+    rr.table('students').get(studentid)('groups').
+        eqJoin((i) => i, rr.table('groups'))("right")("COs").
+        concatMap((i) => i).
+        eqJoin((i) => i, rr.table('COs'))('right').
+        filter(filter).
+        then((e) => { returnfn(e) })
+}
+
