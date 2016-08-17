5caa84f711890397f6aa29269e327a7b7b32766b
Meagan O'Hara <megseoh@gmail.com>
2016-08-17 15:03:01 -0400

    began styling body, added Roboto font, styled header in Student ReadView index.jsx

--- ckx/app/components/Student/ReadView/index.jsx
+++ ckx/app/components/Student/ReadView/index.jsx
@@ -4,16 +4,16 @@ import ObservationList from './ObservationList'
 import ObservationDetail from './ObservationDetail'
 import AddObservation from './AddObservation'
 
-const Header = ({ board, user }) => {return(
-  <div>
-    <h1>{board}</h1>
-    <h1>{user}</h1>
+const Header = ({ board, user }) => {return (
+  <div style = {headerStyle}>
+    <h1 style = {h1Style}>{board}</h1>
+    <h1 style = {h1Style}>{user}</h1>
   </div>
 )}
 
 const ReadView = ({ board, user }) => { return (
   <div>
-    <Header board={board} user={user} /> 
+    <Header board={board} user={user} />
     <ObservationList />
     <ObservationDetail />
     <AddObservation />
@@ -21,3 +21,14 @@ const ReadView = ({ board, user }) => { return (
 )}
 
 export default connect(e => ({board: e.ui.board, user: e.ui.user}))(ReadView)
+
+const headerStyle = {
+  background: '#E44B27',
+}
+
+const h1Style = {
+  color: '#ffffff',
+  fontWeight: '400',
+  display: 'inline-block',
+  padding: '5px 30px'
+}
